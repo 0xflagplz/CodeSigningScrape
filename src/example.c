24f6e112ef26d1d87128097c26a42adff6757011
Ed <59506742+0xflagplz@users.noreply.github.com>
2024-07-07 07:24:51 -0700

    Create example.c

---
+++ src/example.c
@@ -0,0 +1,17 @@
+// Simple C program to display "Hello World"
+//
+// Compile: x86_64-w64-mingw32-gcc example.c -o example.exe
+//
+// Header file for input output functions
+#include <stdio.h>
+ 
+// main function -
+// where the execution of program begins
+int main()
+{
+ 
+    // prints hello world
+    printf("Hello World");
+ 
+    return 0;
+}
